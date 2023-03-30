5c20dbf3303536d612637bbec913415342ef98d8
Kaiden Parcher <ktparcher@.com>
2023-03-29 21:26:57 -0700

    add special; character var

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var SpecialCharacter = {"!", "#", "$" "%", "&", "+",":", "?", "@", ";", "=", "~", }
+var SpecialCharacter = {"!", "#", "$" "%", "&", "+",":", "?", "@", ";", "=", "~", "-", "_"}
 
-!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
 
 var LowerCaseAlphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 var UpperCaseAlphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
