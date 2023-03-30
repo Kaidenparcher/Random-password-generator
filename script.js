af32a0db4cb66df24379730e0638a70fb47bede6
Kaiden Parcher <ktparcher@.com>
2023-03-29 21:23:33 -0700

    add arrays of upper and lower case

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var SpecialCharacter = {"!", "#", "$" "%", "&", "+",":", "?", "@", ";", "=", "~", }
+
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+var LowerCaseAlphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+var UpperCaseAlphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
 
 // Write password to the #password input
 function writePassword() {
