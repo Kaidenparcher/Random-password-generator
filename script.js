ebc9404783427a194e9ad55f3f9b39b7b8c0e25a
Kaiden Parcher <ktparcher@.com>
2023-03-29 22:49:27 -0700

    1. created function generatepassword(). 2. created pop up otions for password length. 3. created pop up otion for lower case.

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 var SpecialCharacter = ["!", "#", "$", "%", "&", "+",":", "?", "@", ";", "=", "~", "-", "_"]
 var Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var LowerCaseAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var UpperCaseAlphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var LowercaseAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var UppercaseAlphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
 
-  passwordText.value = password;
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword(){
+     var password = "";
+     var passwordText = "";
+    // creates user prompt to select password length
+     var passwordLength = prompt("Choose how many characters you want your password to be! Password must be between 8-128 characters.");
+
+        if (passwordLength < 8){
+            alert("Password must have at least 7 characters.");
+            return ""}
+    
+        if (passwordLength > 128) {
+            alert("Password can not have more than 128 characters.");
+            return ""}
+
+
+     var LowercaseOption = confirm("Would you like to include lowercase characters?")
+        
+        if (LowercaseOption){
+            passwordText += LowercaseAlphabet
+        }
+     var UppercaseOption = confrim("Would you like to include Uppercase characters?")
+
+
+
+
+
+
+
+
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
