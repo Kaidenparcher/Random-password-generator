2ec33642a824d5e7903745b2944f85c8ee06e580
Kaiden Parcher <ktparcher@.com>
2023-03-30 17:59:02 -0700

    added console log to double check all inputs

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var SpecialChar = ["!", "#", "$", "%", "&", "+",":", "?", "@", ";", "=", "~", "-", "_"]
-var Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var LowercaseAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var UppercaseAlphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var password = [];
-var passwordText = [];
+var SpecialChar = "!#$%&+:?@;=~-_";
+var Numbers = '0123456789';
+var LowercaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+var UppercaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var userPassword = [];
+var finalPassword = [];
 
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-    
+
+    passwordText.value = password.join("");
+
 }
 
 // Assignment Code
@@ -19,57 +19,64 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-function generatePassword(){
-     
-// creates user prompt to select password length
-     var passwordLength = prompt("Choose how many characters you want your password to be! Password must be between 8-128 characters.");
-//  Converts the string input to integers 
-     passwordLength = parseInt(passwordLength);
-//  Creates password min and max lengeths for total password
-        if (passwordLength < 8){
-            alert("Password must have at least 7 characters.");
-            return ""
-        }
-        if (passwordLength > 128) {
-            alert("Password can not have more than 128 characters.");
-            return ""
-        }
-// Create confrim window for lowercase
+function generatePassword() {
+
+    // creates user prompt to select password length
+    var passwordLength = prompt("Choose how many characters you want your password to be! Password must be between 8-128 characters.");
+    //  Converts the string input to integers 
+
+    //  Creates password min and max lengeths for total password
+    if (passwordLength < 8) {
+        alert("Password must have at least 7 characters.");
+        return ""
+    }
+    if (passwordLength > 128) {
+        alert("Password can not have more than 128 characters.");
+        return ""
+    }
+
+    // Create confrim window for lowercase
     var LowercaseOption = confirm("Would you like to include lowercase characters?");
-        if (LowercaseOption) {
-            passwordText += LowercaseAlphabet;
-        }
-// Create confrim window for Uppercase
+    if (LowercaseOption) {
+       userPassword.push(...LowercaseAlphabet);
+    }
+    console.log("lowercase option",userPassword);
+    
+    // Create confrim window for Uppercase
     var UppercaseOption = confirm("Would you like to include Uppercase characters?");
-            if (UppercaseOption){
-                passwordText += UppercaseAlphabet;
-        }
-// Create confrim window for Special characters
+    if (UppercaseOption) {
+       userPassword.push(...UppercaseAlphabet);
+    }
+    console.log("uppercase option",userPassword);
+    
+    // Create confrim window for Special characters
     var SpecialOption = confirm("Would you like to include Special characters?");
-        if (SpecialOption){
-            passwordText += SpecialChar;
-        }
-// Create confrim window for numbers
+    if (SpecialOption) {
+       userPassword.push(...SpecialChar);
+    }
+    console.log("special option",userPassword);
+    
+    // Create confrim window for numbers
     var NumbersOption = confirm("Would you like to include Special characters?");
-        if (NumbersOption){
-            passwordText += Numbers;
-        }
-// This makes the user at selected at least one option
+    if (NumbersOption) {
+       userPassword.push(...Numbers);
+    }
+    console.log("number option",userPassword);
+    
+    // This makes the user at selected at least one option
     if (!LowercaseOption && !UppercaseOption && !SpecialOption && !NumbersOption) {
         alert("Please choose a criteria");
-
-
-// This is our for function that randomly chooses an input from all of our arrays
-    for (var i = 0; i < passwordLength; i++) {
-           var password = passwordText[Math.floor(Math.random() * passwordText.length)]
-           
-        }
-        return password.join("");
     }
- 
+    
+    // This is our for function that randomly chooses an input from all of our arrays
+    for (var i = 0; i < passwordLength; i++) {
+        finalPassword.push(userPassword[Math.floor(Math.random() * userPassword.length)])
+        console.log('final password', finalPassword)
+    }
+
+    
 }
 
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
